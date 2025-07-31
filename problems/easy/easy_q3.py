ad0b68e5c428dcebe6d5da65c01fb36054af61ba
thaksmitha <thaksmitha1826@gmail.com>
2025-07-31 11:53:20 +0530

    error changed

--- problems/easy/easy_q3.py
+++ problems/easy/easy_q3.py
@@ -1,7 +1,7 @@
 # Leap Year or Not: Write a program to determine whether a given year is a leap year.
 def is_leap_year(year):
  
-    if year % 4 == 0:
+    if year % 4  == 0 and year % 100 != 0 or year % 400 == 0:
         print("leap year")
     else:
         print("not a leap year")
@@ -11,10 +11,4 @@ if __name__ == "__main__":
     num = int(input("Enter the number :"))
     is_leap_year(num)
  
-    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:   
- 
-        return "Leap Year"
-    else:   
- 
-        return "Not a Leap Year"
- 
+    
