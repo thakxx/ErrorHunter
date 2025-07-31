7917915d2be22a03296bede5513ab0072e6718de
thaksmitha <thaksmitha1826@gmail.com>
2025-07-31 11:28:47 +0530

    error changed

--- problems/easy/easy_q2.py
+++ problems/easy/easy_q2.py
@@ -22,36 +22,12 @@ def largest_of_two(a, b):
     else:
         return b
  
-    if a < b:
-        return b   
-    else:
-        return a
-
+    
 num1 = int(input("Enter the First Number :"))
 num2 = int(input("Enter the Second Number :"))
 res = largest_of_two(num1,num1)
 print(res)
  
-    if a > b:
+    
  
-        return a  
-    else:
-        return b
- 
- 
-        return a  
-    else:
-        return b
- 
-        return a   
-    else:
-
-        return b
- 
- 
-if __name__ == "__main__":
-    num1 = int(input("Enter the First Number :"))
-    num2 = int(input("Enter the Second Number :"))
-    res = largest_of_two(num1,num2)
-    print(res)
  
